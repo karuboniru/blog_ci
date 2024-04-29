a8ef09b338a491fa8ca8d5dfb58d2012458828ee
Karuboniru <yanqiyu01@gmail.com>
2024-04-29 15:43:45 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 curl -s -L https://github.com/jgm/pandoc/releases/download/3.1.13/pandoc-3.1.13-linux-amd64.tar.gz | tar xvzf - -C ./
-# npm i -g hexo-cli gulp gulp-uglify
+npm i -g hexo-cli gulp gulp-uglify
 # npm i
 export PATH="$(pwd)/pandoc-3.1.13/bin:$PATH"
 hexo generate && gulp build
