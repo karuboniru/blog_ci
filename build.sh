a79106f23fc307c5832dbd1a23734490df6f7726
Karuboniru <yanqiyu01@gmail.com>
2024-04-29 15:24:41 +0100

    Update build.sh (#99)

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-curl -s -L https://github.com/jgm/pandoc/releases/download/2.9.2.1/pandoc-2.9.2.1-linux-amd64.tar.gz | tar xvzf - -C ./
+curl -s -L https://github.com/jgm/pandoc/releases/download/3.1.13/pandoc-3.1.13-linux-amd64.tar.gz | tar xvzf - -C ./
 # npm i -g hexo-cli gulp gulp-uglify
 # npm i
 export PATH="$(pwd)/pandoc-2.9.2.1/bin:$PATH"
