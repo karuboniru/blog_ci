ee306d9ca2aa58770bfa732a2817e598f1955c57
Qiyu Yan <yanqiyu@fedoraproject.org>
2026-09-09 14:06:00 +0800

    Minify all site CSS/JS in the gulp build
    Replace unmaintained uglify-es (cannot parse ?. / ??) with terser and
    add gulp-clean-css. Minification runs before workbox injectManifest so
    precache revision hashes match the minified files; sw.js is still
    minified last since it is generated afterwards and not precached.

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,27 @@
 const gulp = require("gulp");
 const workbox = require("workbox-build");
-const uglifyes = require("uglify-es");
-const composer = require("gulp-uglify/composer");
-const uglify = composer(uglifyes, console);
+const terser = require("gulp-terser");
+const cleanCSS = require("gulp-clean-css");
 const pipeline = require("readable-stream").pipeline;
+
+// 压缩站点全部 JS/CSS。必须在 generate-service-worker 之前运行：
+// injectManifest 按文件内容计算预缓存 revision 哈希，压缩后再生成
+// 清单才能保证 Service Worker 缓存的哈希与实际文件一致。
+gulp.task("minify-js", () => {
+  return pipeline(
+    gulp.src("./public/**/*.js"),
+    terser(),
+    gulp.dest("./public"),
+  );
+});
+
+gulp.task("minify-css", () => {
+  return pipeline(
+    gulp.src("./public/**/*.css"),
+    cleanCSS(),
+    gulp.dest("./public"),
+  );
+});
 
 gulp.task("generate-service-worker", () => {
   return workbox.injectManifest({
@@ -18,12 +36,20 @@ gulp.task("generate-service-worker", () => {
   });
 });
 
+// sw.js 在 injectManifest 之后才生成，且不在预缓存清单内，故单独压缩。
 gulp.task("uglify", function () {
   return pipeline(
     gulp.src("./public/sw.js"),
-    uglify(),
+    terser(),
     gulp.dest("./public"),
   );
 });
 
-gulp.task("build", gulp.series("generate-service-worker", "uglify"));
+gulp.task(
+  "build",
+  gulp.series(
+    gulp.parallel("minify-js", "minify-css"),
+    "generate-service-worker",
+    "uglify",
+  ),
+);
